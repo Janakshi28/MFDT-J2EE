b1fb92e7dc88ce05fb5ea59bd763de01d1b3fb0f
Janakshi Dulanga <janakshid28@gmail.com>
2017-03-03 08:59:42 +0530

    Update LockerReservationFacade.java
    Adding the business logic that needs to perform a locker reservation to the stateful session bean. This session bean holds the state of a customer's locker reservations.

--- MegaFunDTPark-ejb/src/java/lk/megafundtpark/sessionbeans/LockerReservationFacade.java
+++ MegaFunDTPark-ejb/src/java/lk/megafundtpark/sessionbeans/LockerReservationFacade.java
@@ -5,15 +5,23 @@
  */
 package lk.megafundtpark.sessionbeans;
 
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.ejb.Remove;
 import lk.megafundtpark.remote.interfaces.LockerReservationFacadeRemote;
 import javax.ejb.Stateful;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import lk.megafundtpark.remote.interfaces.EbandsFacadeRemote;
+import lk.megafundtpark.remote.interfaces.ElockersFacadeRemote;
+import lk.megafundtpark.remote.interfaces.PayablesFacadeRemote;
 
 /**
  *
@@ -21,7 +29,12 @@ import javax.persistence.PersistenceContext;
  */
 @Stateful(mappedName = "lockerreservationfacade")
 public class LockerReservationFacade implements LockerReservationFacadeRemote {
-        
+    private EbandsFacadeRemote bandHandler;
+    private ElockersFacadeRemote lockersHandler;
+    private PayablesFacadeRemote payableHandler;
+    
+
+    private HashMap<String, List<String>> lockerInfo= null;    
 
     @PersistenceContext(unitName = "MegaFunDTPark-ejbPU")
     private EntityManager em;
@@ -31,9 +44,90 @@ public class LockerReservationFacade implements LockerReservationFacadeRemote {
         em.persist(object);
     }
 
-	@Remove
+    @PostConstruct
+    private void initializeBean() {
+        lockerInfo = new HashMap<>();
+        try {
+            InitialContext ic = new InitialContext();
+            bandHandler = (EbandsFacadeRemote) ic.lookup("ebandsfacade");
+            lockersHandler = (ElockersFacadeRemote) ic.lookup("elockersfacade");
+            payableHandler= (PayablesFacadeRemote)ic.lookup("payablesfacade");
+        } catch (NamingException ex) {
+            Logger.getLogger(LockerReservationFacade.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+	
+    
+    public void addNewReservation(String lockerId) {
+        lockerInfo.put(lockerId, new ArrayList<String>());
+    }
+
+    
+    public void addNewReservation(String bandId, String lockerId) {
+        List<String> list = new ArrayList<>();
+        list.add(lockerId);
+        lockerInfo.put(bandId, list);
+    }
+
+    
+    public void reserveLocker(String bandId, String lockerId) {
+        List<String> lockerList = lockerInfo.get(bandId);
+        if (lockerList == null) {
+            this.addNewReservation(bandId, lockerId);
+        } else {
+            lockerList.add(lockerId);
+            lockerInfo.put(bandId, lockerList);
+        }
+    }
+
+    
+    public void clearLocker(String bandId, String lockerId) {
+        List<String> lockerList = lockerInfo.get(bandId);
+        if (lockerList != null) {
+            lockerList.remove(lockerId);
+            lockerInfo.put(bandId, lockerList);
+        }
+    }
+    
+    
+    
+    public List<String> getReservedLockerList(String bandId) {
+        return lockerInfo.get(bandId);
+    }
+
+    
+    public void removeEBand(String bandId) {
+        lockerInfo.remove(bandId);
+    }
+
+    
+    
+    public int getLockersPerEBand(String bandId) {
+        List<String> lockers = lockerInfo.get(bandId);
+        if (lockers != null) {
+            return lockers.size();
+        } else {
+            return 0;
+        }
+    }
+    
+    
+    public int getAllReservedLockerCounter() {
+        int lockerNumber = 0;
+        Set<String> keys = lockerInfo.keySet();
+        for(String key : keys) {
+            lockerNumber += lockerInfo.get(key).size();
+        }
+        
+        return lockerNumber;
+    }
+
+    @Remove
     @Override
-    public void remove() {        
+    public void remove() {
+	lockerInfo.clear();
+        lockerInfo = null;
     }
     
 }
